66ddaaa796ed83c365fe01cc6803f376ad80be23
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2023-11-20 20:21:35 +0530

    Code Updated

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,13 +1,21 @@
 package com.driver;
 
-public class BankAccount {
+/* Some Important keyword definations
+final: The final keyword is a non-access modifier used for classes, attributes and methods, which makes them non-changeable 
+(impossible to inherit or override).
+this: the this keyword refers to the current object in a method/constructor.
 
-    private String name;
-    private double balance;
-    private double minBalance;
+*/
+
+public class BankAccount {
+    private final String name; //non-changeable(Fixed username)
+    private double balance; //changeable(bcoz of amount adding & withdrawing)
+    private final double minBalance; //non-changeable(fixed balance amt after withdrwaing/adding)
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
@@ -15,17 +23,57 @@ public class BankAccount {
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        return null;
+        //ex: digits = 5, sum = 23 --> accountNo = 99500 => 23
+        if (sum < 0 || sum > 9*digits) {
+           throw new Exception("Account Number can not be generated");
+        } else {
+            //generating account num of sumsize
+            StringBuilder accountNo = new StringBuilder();
+            while (sum > 9) {
+                accountNo.append("9");
+                sum -= 9;
+            }
+
+            while (accountNo.length() < digits) {
+                accountNo.append(sum);
+                sum = 0;
+            }
+            return accountNo.toString();
+        }
+
     }
 
-    public void deposit(double amount) {
-        //add amount to balance
-
+    public void deposit(double amount) { //add amount to balance
+        // We are tring to depositing amount, so we check if the amount is > 0 then we add with currentBalance
+        if (amount > 0) {
+            balance = balance + amount;
+        }
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        // here we are trying to withdrawing the amount, we have to take care minbalance also. 
+        // we check if the withdrawing amount is >= minBalance
+        if (balance - amount >= minBalance) {
+            balance -= amount;
+        } else {
+            throw new Exception("Insufficient Balance");
+        }
+    }
 
+    // Getter
+    public String getName() {
+        return name;
+    }
+    
+    // Getter
+    public double getMinBalance() {
+        return minBalance;
+    }
+    
+    // Getter
+    public double getBalance() {
+        return balance;
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -3,8 +3,17 @@ package com.driver;
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    public String getTradeLicenceID() {
+        return tradeLicenseId;
+    }
+
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000.0);
+        this.tradeLicenseId = tradeLicenseId;
+        if(balance < 5000) {
+            throw new Exception("Insufficient Balance");
+        }
 
     }
 
@@ -14,6 +23,81 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        if(!isValid(tradeLicenseId)) {
+            String rearrange = RearrangeString(tradeLicenseId);
+
+            if(rearrange == "") {
+                throw new Exception("Valid License can not be generated");
+            } else {
+                this.tradeLicenseId = rearrange;
+            }
+        }
+    }
+
+    public String RearrangeString(String id) {
+        int n = id.length();
+        int[] cnt = new int[26];
+
+        for(int i = 0; i < 26; i++) {
+            cnt[i] = 0;
+        }
+
+        for(char c : id.toCharArray()) {
+            cnt[(int)c-(int)'A']++;
+        }
+
+        char c_max = getCharCnt(cnt);
+        int max_cnt = cnt[(int)c_max-(int)'A'];
+
+        if(max_cnt > (n+1)/2) {
+            return "";
+        }
+
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            res += ' ';
+        }
+
+        int idx = 0;
+        while(max_cnt > 0) {
+            res = res.substring(0, idx) + c_max + res.substring(idx + 1); 
+            idx += 2;
+            max_cnt--;
+        }
+        cnt[(int)c_max-(int)'A'] = 0;
+        for(int i = 0; i < 26; i++) {
+            while(cnt[i] > 0) {
+                idx = (idx >= 1) ? 1 : idx;
+                res = res.substring(0, idx) + (char)((int)'A' + i) + res.substring(idx + 1); 
+                idx += 2;
+                cnt[i]--;
+            }
+        }
+        return res;
+    }
+
+    public char getCharCnt(int[] cnt) {
+        int maxi = 0;
+        char ch = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if(cnt[i] > maxi) {
+                maxi = cnt[i];
+                ch = (char)((int)'A' + i);
+            }
+        }
+        return ch;
+    }
+
+    public boolean isValid(String id) {
+        if(id.length() == 0) return false;
+
+        for(int i = 0; i < id.length()-1; i++) {
+            if(id.charAt(i) == id.charAt(+1)){
+                return false;
+            } 
+        }
+        return true;
     }
 
 }
